749d2873ff3db9f8448e71e7f93f26cc118a99c0
Mayank <mayank.srmu@gmail.com>
2024-06-25 20:36:25 +0530

    Display full screen

--- lib/src/client/mouse-trail/mouse-trail.tsx
+++ lib/src/client/mouse-trail/mouse-trail.tsx
@@ -1,25 +1,19 @@
 import { HTMLProps, ReactNode } from "react";
 import styles from "./mouse-trail.module.scss";
 
-export interface MouseTrailProps extends HTMLProps<HTMLDivElement> {
-	children?: ReactNode;
+export interface MouseTrailProps extends HTMLProps<HTMLCanvasElement> {
+  children?: ReactNode;
 }
 
 /**
- * 
+ *
  *
  * @example
  * ```tsx
  * <MouseTrail />
  * ```
- * 
- * @source - Source code
  */
-export const MouseTrail = ({ children, ...props }: MouseTrailProps) => {
-  const className = [props.className, styles["mouse-trail"]].filter(Boolean).join(" ");
-	return (
-		<div {...props} className={className} data-testid="mouse-trail">
-			{children}
-		</div>
-	);
-}
+export const MouseTrail = (props: MouseTrailProps) => {
+  const className = [styles.trail, props.className ?? ""].join(" ");
+  return <canvas {...props} className={className} data-testid="mouse-trail" />;
+};
